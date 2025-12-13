964eab9700903e9b19e3b23a8589dec2d718bd40
Christeen Paul <chrispaul1311@gmail.com>
2025-12-13 13:27:09 +0530

    vercel-deploy

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true
 }));
 
